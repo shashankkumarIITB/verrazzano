53f9caba35e41821bd26932f0dff97cf483dad2f
Michael Cico <46466506+michael-cico@users.noreply.github.com>
2022-06-14 16:15:53 -0400

    VZ-6098. Add a short-circuit check of the current component state in its' status field in the dependency check. - Change debug output for dependency check

--- platform-operator/controllers/verrazzano/component/registry/registry.go
+++ platform-operator/controllers/verrazzano/component/registry/registry.go
@@ -4,6 +4,7 @@
 package registry
 
 import (
+	vzapi "github.com/verrazzano/verrazzano/platform-operator/apis/verrazzano/v1alpha1"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/appoper"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/authproxy"
 	"github.com/verrazzano/verrazzano/platform-operator/controllers/verrazzano/component/certmanager"
@@ -147,11 +148,25 @@ func checkDependencies(c spi.Component, context spi.ComponentContext, visited ma
 			return trace, err
 		}
 		// Only check if dependency is ready when the dependency is enabled
-		if dependency.IsEnabled(context.EffectiveCR()) && !dependency.IsReady(context) {
+		if !isInReadyState(context, dependency) && // CR status does not already indicate ready status
+			dependency.IsEnabled(context.EffectiveCR()) && !dependency.IsReady(context) {
 			stateMap[dependencyName] = false // dependency is not ready
 			continue
 		}
 		stateMap[dependencyName] = true // dependency is ready
 	}
 	return stateMap, nil
+}
+
+func isInReadyState(context spi.ComponentContext, comp spi.Component) bool {
+	dependencyStatus, ok := context.ActualCR().Status.Components[comp.Name()]
+	if ok {
+		context.Log().Infof("Dependency %s state: %s", comp.Name(), dependencyStatus.State)
+		// We've already reported Ready status for this component
+		if dependencyStatus.State == vzapi.CompStateReady {
+			context.Log().Infof("Dependency %s in Ready state", comp.Name())
+			return true
+		}
+	}
+	return false
 }
